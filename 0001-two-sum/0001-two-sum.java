2b19c325677ee2e2dbb3ee0735eef5b1a9340787
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2024-07-06 21:17:17 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int a[] = new int[2];
-        for(int i=0; i < nums.length; i++)
-        {
-            for(int j=i+1; j < nums.length; j++)
-            {
-                if(nums[i]+nums[j] == target)
-                {
-                    a[0] = i;
-                    a[1] = j;
+       for(int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    int a[] = {i,j};
                     return a;
                 }
             }
-            
         }
-        return a;
+        return null;
     }
 }
