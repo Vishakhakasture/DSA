c472654854da65e464d473d2eae179d226322cdf
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2024-07-13 23:29:03 +0530

    Time: 51 ms (56.66%), Space: 48.9 MB (25.60%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,10 +3,8 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let currentCount = n - 1;
-  return function() {
-    currentCount += 1;
-    return currentCount;      
+    return function() {
+    return n++;      
   };
 };
 
