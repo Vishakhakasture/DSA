22e0a62f83bbae95fabb20efed0cc8c942f8936d
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2024-07-26 16:34:15 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (42.58%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,49 +8,33 @@ if(nums[mid] > nums[mid+1]) then pivot = nums[mid]
 
 class Solution {
     public int search(int[] nums, int target) {
-        int n = nums.length - 1;
-        int pivot = findPivot(nums,n);
-        if(pivot == -1) {
-            return binarySearch(nums, target, 0, n);
-        }
-        if(target == nums[pivot]) {
-            return pivot;
-        }
-        if(target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-        return binarySearch(nums, target, pivot+1, n);
-    }
-    public int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-            if(target < arr[mid]) {
-                end = mid - 1;
-            } else if(target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-    public int findPivot(int[] nums, int end) {
         int start = 0;
+        int end = nums.length - 1;
+
         while(start <= end) {
-            int mid = start + (end - start) / 2;
-            if(mid < end && nums[mid] > nums[mid+1]) {
+            int mid = (start + end)/2;
+
+            if(target == nums[mid]) {
                 return mid;
             }
-            if(mid > start && nums[mid] < nums[mid-1]) {
-                return mid-1;
-            }
-            if(nums[mid] <= nums[start]) {
-                end = mid - 1;
-            } 
-            else {
-                start = mid + 1;
+
+            if(nums[start] <= nums[mid]) { // left array is sorted
+                if(target >= nums[start] && target < nums[mid]) { // target lies between start and mid index
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            } else { // right array is sorted
+                if(target > nums[mid] && target <= nums[end]) { // target lies between mid and end index
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
             }
         }
+
         return -1;
     }
+
+
 }
