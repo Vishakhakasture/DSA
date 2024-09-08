f05ca569df73deed4fc2c5a49da726551571bfd3
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2024-09-09 00:08:17 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (64.79%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,11 +1,11 @@
 class Solution {
     public String toLowerCase(String s) {
-     // char c[] = s.toCharArray();
-     //    for(int i = 0; i < c.length; i++) {
-     //        if(c[i] >= 65 && c[i] <= 90) 
-     //            c[i] = (char) (c[i] + 32);
-     //    }
-     //    return String.valueOf(c);
-        return s.toLowerCase();
+     char c[] = s.toCharArray();
+        for(int i = 0; i < c.length; i++) {
+            if(c[i] >= 65 && c[i] <= 90) 
+                c[i] = (char) (c[i] + 32);
+        }
+        return String.valueOf(c);
+        // return s.toLowerCase();
     }
 }
