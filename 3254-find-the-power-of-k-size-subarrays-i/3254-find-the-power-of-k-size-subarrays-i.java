67ad075948770f6f09c4cbfeca268c090e790af8
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2024-11-16 16:55:02 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (81.91%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
+        if(k == 1) {
+            return nums;
+        }
         int n = nums.length;
         int ans[] = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-        for(int i = 0; i < n; i++) {
-            if(!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
-                dq.pollFirst();
+        Arrays.fill(ans, -1);
+        int count = 1;
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i]+1 == nums[i+1]) {
+                count++;
+            } else {
+                count = 1;
             }
-            if(!dq.isEmpty() && nums[i] != nums[i-1]+1) {
-                dq.clear();
-            }
-            dq.offerLast(i);
-            if(i >= k - 1) {
-                if(dq.size() == k) {
-                    ans[i-k+1] = nums[dq.peekLast()];
-                } else {
-                    ans[i-k+1] = -1;
-                }
+            if(count >= k) {
+                ans[i-k+2] = nums[i+1];
             }
         }
         return ans;
