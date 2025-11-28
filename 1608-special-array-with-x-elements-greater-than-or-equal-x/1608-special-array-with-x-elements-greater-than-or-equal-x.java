56a3d549ffd9ab547cb9040381bfb0d7600b0111
vishakha kasture <114031393+Vishakhakasture@users.noreply.github.com>
2025-11-28 17:10:06 +0530

    Time: 5 ms (10.8%), Space: 43.1 MB (27.81%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -5,9 +5,26 @@ class Solution {
 
         if(nums[0] >= n) return n;
 
-        for(int i = 1; i < n; i++) {
-            if(nums[n - i] >= i && (n - i - 1 < 0 || nums[n - i - 1] < i)) {
-                return i;
+        // for(int i = 1; i < n; i++) {
+        // if(nums[n - i] >= i && (n - i - 1 < 0 || nums[n - i - 1] < i)) {
+        //         return i;
+        //     }
+        // }
+        int low = 0, high = n;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            for(int num : nums) {
+                if(num >= mid) {
+                    count++;
+                }
+            }
+            if(count == mid) {
+                return mid;
+            } else if(count > mid) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
